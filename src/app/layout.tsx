93550ac788c0df0a10692e0d9ae103a1e5754bf6
Fatih Kurt <lifelonglearnerest@gmail.com>
2024-04-07 18:01:49 +0300

    feat: pwa add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Nobstacle",
   description: "Nobstacle",
+  manifest: "/manifest.json",
 };
 
 interface RootLayourPropsI {
