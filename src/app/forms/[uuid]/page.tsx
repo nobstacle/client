a1d5d6a61fb295d5a1cfb4f42ddf3914ae1c2b15
deepatatAleait <deepak@aleaitsolutions.com>
2025-05-09 16:55:34 +0530

    NOB bug udpate

--- src/app/forms/[uuid]/page.tsx
+++ src/app/forms/[uuid]/page.tsx
@@ -92,7 +92,7 @@ const FormByUUID = () => {
     <div style={{ background: 'white', height: '100vh', width: '100%' }}>
       {error ? (
         <p style={{ color: 'red' }}>Error: {error}</p>
-      ) : !formData ? (
+      ) : formData ? (
         <div style={{ width: '100%', height: '100vh' }}>
           {formUrl && (
             <iframe
