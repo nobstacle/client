cd98ccd1e1fefdf1bd0acf1ccfb1e17b85a6cc8e
deepatatAleait <deepak@aleaitsolutions.com>
2026-05-13 12:06:30 +0530

    NOB deployment fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,7 @@ import Extension from '@/components/home/Extension';
 import ScrollToTop from '@/components/home/ScrollToTop';
 import "@/styles/home.css";
 
+
 export default function Home() {
   const [privacyModalOpen, setPrivacyModalOpen] = useState(false);
   const [termsModalOpen, setTermsModalOpen] = useState(false);
